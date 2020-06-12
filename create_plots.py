1ccaf425f9d24fdbaad4778aa15974eeaf8c5556
Pawel Ksieniewicz <pawel@ksienie.com>
2020-06-12 18:04:38 +0200

    EPS

--- create_plots.py
+++ create_plots.py
@@ -78,36 +78,36 @@ for ind, fname in enumerate(fnames):
             [
                 (1, 0.8, 0.8),
                 (0.8, 0.8, 1),
-                "red",
-                "blue",
-                "red",
-                "blue",
-                "red",
-                "blue",
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
             ],
             [
                 (1, 0.7, 0.7),
                 (0.7, 0.7, 1),
                 (1, 0.7, 0.7),
                 (0.7, 0.7, 1),
-                "red",
-                "blue",
-                "red",
-                "blue",
-                "red",
-                "blue",
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
             ],
             [
                 (1, 0.7, 0.7),
                 (0.7, 0.7, 1),
-                (1, 0.7, 0.7),
+                (1, 0.5, 0.5),
                 (0.7, 0.7, 1),
-                "red",
-                "blue",
-                "red",
-                "blue",
-                "red",
-                "blue",
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
+                (1, 0.4, 0.3),
+                (0.3, 0.4, 1),
             ],
         ]
         ls = [
@@ -131,7 +131,7 @@ for ind, fname in enumerate(fnames):
 
         print(new_results.shape)
 
-        fig, ax = plt.subplots(3, 1, figsize=(8, 11))
+        fig, ax = plt.subplots(3, 1, figsize=(6.5, 9))
         for i in range(3):
             for j, row in enumerate(new_results[usages[i], :, m_i]):
                 # res = anisotropic_diffusion(row, gamma=0.05, kappa=300, niter=10)
@@ -143,13 +143,13 @@ for ind, fname in enumerate(fnames):
                     res,
                     c=colors[i][j],
                     ls=ls[i][j],
-                    label=labels[usages[i][j]],
+                    label="%s\n$^{%.3f}$" % (labels[usages[i][j]], np.mean(row)),
                     lw=lw[i][j],
                 )
             ax[i].set_title(titles[i])
             ax[i].set_ylim(0, 1)
             ax[i].set_xlim(0, 100 - 1)
-            ax[i].legend(ncol=len(usages[i]) // 2, frameon=False)
+            ax[i].legend(ncol=len(usages[i]) // 2, frameon=False, fontsize=10)
             ax[i].spines["right"].set_color("none")
             ax[i].spines["top"].set_color("none")
             ax[i].grid(ls=":")
@@ -159,45 +159,84 @@ for ind, fname in enumerate(fnames):
 
         # plt.tight_layout()
         plt.savefig("foo.png")
-        plt.savefig("figures/%s.png" % (fname + " " + m))
-
-        # exit()
+        plt.savefig("figures/%s.png" % (fname + "_" + m))
+        plt.savefig("figures/eps/%s.eps" % (fname + "_" + m))
 
     # Comparision of selected (the best) proposed models with WAE, OOB and UOB
     colors = [
-        ["dodgerblue", "cyan", "darkviolet", "mediumslateblue", "green", "blue", "red"],
-        ["dodgerblue", "cyan", "darkviolet", "mediumslateblue", "green", "blue", "red"],
-        ["dodgerblue", "cyan", "darkviolet", "mediumslateblue", "green", "blue", "red"],
+        [
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+        ],
+        [
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+        ],
+        [
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+            (0.3, 0.7, 0.3),
+            (0.4, 0.3, 1),
+        ],
+    ]
+    ls = [
+        ["-", "-", ":", ":", "--", "--", "-.", "-."],
+        ["-", "-", ":", ":", "--", "--", "-.", "-."],
+        ["-", "-", ":", ":", "--", "--", "-.", "-."],
     ]
 
     usages = [
-        [9, 10, 11, 26, 12, 13, 14],
-        [9, 10, 11, 26, 12, 13, 14],
-        [9, 10, 11, 26, 12, 13, 14],
+        [26, 12, 9, 13, 10, 14, 11],
+        [26, 12, 9, 13, 10, 14, 11],
+        [26, 12, 9, 13, 10, 14, 11],
     ]
 
     lw = [
-        [2, 2, 2, 2, 1, 1, 1],
-        [2, 2, 2, 2, 1, 1, 1],
-        [2, 2, 2, 2, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
     ]
     locs = [1, 3, 3]
 
     print(new_results.shape)
 
-    fig, ax = plt.subplots(3, 1, figsize=(8, 11))
+    fig, ax = plt.subplots(3, 1, figsize=(6.5, 9))
     for i in range(3):
         for j, row in enumerate(new_results[usages[i], :, i]):
             res = gaussian_filter(row, kernel)
             ax[i].plot(
-                res, c=colors[i][j], ls="-", label=labels[usages[i][j]], lw=lw[i][j]
+                res,
+                c=colors[i][j],
+                ls=ls[i][j],
+                label="%s\n$^{%.3f}$" % (labels[usages[i][j]], np.mean(row)),
+                lw=lw[i][j],
             )
-        ax[i].set_title(metrics[i])
+        ax[i].set_title("%s metric" % metrics[i])
         ax[i].set_ylim(0, 1)
-        ax[i].legend(ncol=3, frameon=False)
+        ax[i].legend(ncol=4, frameon=False)
+        ax[i].spines["right"].set_color("none")
+        ax[i].spines["top"].set_color("none")
+        ax[i].grid(ls=":")
 
-    fig.suptitle(fname)
+        ax[i].set_xlim(0, 100 - 1)
+
+    fig.suptitle(ftitles[fname] + ".")
+    fig.subplots_adjust(top=0.93, bottom=0.03, left=0.06, right=0.97)
+
     # plt.tight_layout()
     plt.savefig("bar.png")
     plt.savefig("figures/%s.png" % fname)
-    # exit()
+    plt.savefig("figures/eps/%s.eps" % fname)
